0aa40f550eaac016edc07212e430b21aff8bdd5b
Jack <ayinjun1109@163.com>
2022-03-23 21:32:29 +0800

    Job Decider and Job-Step-Job

---
+++ src/main/java/com/ityj/batch/decider/MyJobExecutionDecider.java
@@ -0,0 +1,24 @@
+package com.ityj.batch.decider;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+
+import java.util.Map;
+
+@Slf4j
+public class MyJobExecutionDecider  implements JobExecutionDecider {
+    @Override
+    public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+        Map<String, JobParameter> parameters = jobExecution.getJobParameters().getParameters();
+        log.info("Job Parameter is: {}", parameters);
+
+        if (parameters.containsKey("skip")) {
+            return new FlowExecutionStatus("SKIP");
+        }
+        return new FlowExecutionStatus("CONTINUE");
+    }
+}
